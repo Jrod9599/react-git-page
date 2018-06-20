ee9e464e4a4794bd2e04479dca38599bb5774960
Jrod9599 <Jrod9599@gmail.com>
2018-06-20 17:03:12 -0400

    found error

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-         <div class="progress">
-          <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
-          In the works of making something cool!
+        <div className="progress">
+          <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
         </div>
         Hello World
       </div>
