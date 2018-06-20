253dacf0822b07dcbd3c31a55f8c7bfec49dc946
Jrod9599 <Jrod9599@gmail.com>
2018-06-20 16:57:25 -0400

    rebuilt

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
           In the works of making something cool!
         </div>
+        Hello World
       </div>
     );
   }
