d88eeba8a3abc3a28de9b30c44fcea6fb0f025ce
Jrod9599 <Jrod9599@gmail.com>
2018-06-20 17:09:49 -0400

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="progress">
-          <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+      <Router>
+        <div className="App">
+          <div className="progress">
+            <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+          </div>
+          Hello World
         </div>
-        Hello World
-      </div>
+      </Router>
     );
   }
 }
