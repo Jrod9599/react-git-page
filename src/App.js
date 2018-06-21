494d00639c74c6ee81ab51f3fdc7eaea16ce465f
Jrod9599 <Jrod9599@gmail.com>
2018-06-21 10:59:43 -0400

    loading page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import LoadingPage from './LoadingPage';
+
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <div className="progress">
-            <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
-          </div>
-          Hello World
+        <div>
+          <Route exact path="/" component={LoadingPage} />
         </div>
       </Router>
     );
---
+++ src/LoadingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class LoadingPage extends React.Component {
+
+
+
+	render(){
+
+		return(
+			<div className="centerBox">
+				Page Coming Soon!
+				<div class="progress progressBarBox">
+	  				<div class="progress-bar progress-bar-striped progress-bar-animated" id="pageComplete" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100"></div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default LoadingPage;
